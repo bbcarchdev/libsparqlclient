9f77cedaa3337a34e99250c1ffca355f0d233dba
Mo McRoberts <mo.mcroberts@bbc.co.uk>
2014-08-29 13:04:30 +0100

    When performing an UPDATE, Content-Length must be the length of the triples post-urlencoding.

--- update.c
+++ update.c
@@ -52,7 +52,7 @@ sparql_update(SPARQL *connection, const char *statement, size_t length)
 	curl_easy_setopt(ch, CURLOPT_URL, connection->update_uri);	
 	curl_easy_setopt(ch, CURLOPT_POST, 1);
 	curl_easy_setopt(ch, CURLOPT_POSTFIELDS, buf);
-	curl_easy_setopt(ch, CURLOPT_POSTFIELDSIZE, length + 7);
+	curl_easy_setopt(ch, CURLOPT_POSTFIELDSIZE, strlen(buf));
 	curl_easy_perform(ch);
 	curl_easy_cleanup(ch);
 	free(buf);
